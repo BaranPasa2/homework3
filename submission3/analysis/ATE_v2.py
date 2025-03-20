9a4471aeea36a84e6822c007fdd49e29cba6421c
Baran <baran.pasa@emory.edu>
2025-03-20 16:06:59 -0400

    more updates

--- submission3/analysis/ATE_v2.py
+++ submission3/analysis/ATE_v2.py
@@ -20,7 +20,7 @@ df_91_15 = df[(df['Year'] >= 1991) & (df['Year'] <= 2015)].copy()
 #print(df_70_90.columns)
 #print(df_70_90.index)
 
-# -- Q 6 --
+# -- Q 6 -- GETTING THE CORRECT VALUE
 ols_70_90 = pf.feols('log_sales ~ log_price', data=df_70_90)
 # Extract the coefficient for  and print it
 print(f"{ols_70_90.coef()['log_price']:.4f}")
@@ -28,27 +28,41 @@ print(f"{ols_70_90.coef()['log_price']:.4f}")
 #new_row = pd.DataFrame([{'Variable': 'Log Price', 'Coefficient': ols_70_90.coef()['log_price']}])
 #coef_table = pd.concat([coef_table, new_row], ignore_index=True)
 
-# -- Q 7 --
+# -- Q 7 -- LOOKING FOR -0.796
 iv_70_90 = pf.feols('log_sales ~ 1 | log_price ~ log_total_tax', data=df_70_90)
 print(iv_70_90.summary())
-#print(f"{iv_70_90.coef()['log_price']:.4f}")
 
+etable = pf.etable(
+    [ols_70_90, iv_70_90],
+    omit_stats="all",
+    title=["OLS: log_sales ~ log_price", "IV: log_sales ~ log_price (instrumented)"],
+    subheaders=["No instrument", "Instrument: total_tax"]
+)
+
+print(etable)
 # -- Q 8 --
-stage1 = pf.feols('log_price ~ tax_dollar', data=df_70_90)
+stage1 = pf.feols('log_price ~ log_total_tax', data=df_70_90)
 price_hat = stage1.predict()
 df_70_90['price_hat'] = price_hat
 stage2 = pf.feols('log_sales ~ price_hat', data=df_70_90)
 print(f"First Stage: {stage1.summary()}")
-print(f"Second Stage: {stage2.summary()}")
 
-# -- Q 9 --
+reduced_reg = pf.feols('log_sales ~ log_total_tax', data=df_70_90)
+print(f"Reduced Form: {reduced_reg.summary()}")
+#print(f"Second Stage: {stage2.summary()}")
+
+# -- Q 9 -- 
 ols_91_15 = pf.feols('log_sales ~ log_price', data=df_91_15)
 iv_91_15 = pf.feols('log_sales ~ 1 | log_price ~ log_total_tax', data=df_91_15)
-print(f"{ols_91_15.coef()['log_price']:.4f}")
-print(f"{iv_91_15.coef()['log_price']:.4f}")
-
 
 stage1 = pf.feols('log_price ~ log_total_tax', data=df_91_15)
 price_hat = stage1.predict()
 df_91_15['price_hat'] = price_hat
 stage2 = pf.feols('log_sales ~ price_hat', data=df_91_15)
+
+pf.etable(
+    [ols_70_90, iv_70_90, ols_91_15, iv_91_15],
+    omit_stats="all",
+    title=["OLS: log_sales ~ log_price", "IV: log_sales ~ log_price (instrumented)"],
+    subheaders=["No instrument", "Instrument: total_tax"]
+)
